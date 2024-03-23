2508314740f2c29fe1fdcf46712fbec91abcc34d
ajabbar <ajabbar@Z2P2XISPX0045.corp.premierinc.com>
2024-03-23 13:34:15 -0400

    Second Commit

--- JavaClass2.java
+++ JavaClass2.java
@@ -1,10 +1,9 @@
 
-public class firstClass {
+public class JavaClass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
-		System.out.println("Test1");
+		System.out.println("Second Class");
 	}
 
 }

800722dc7dd9ed41c8046dce93af5f4525b06380
Jabbar-Git <115821810+Jabbar-Git@users.noreply.github.com>
2024-03-23 13:42:56 -0400

    Update JavaClass2.java
    Added Third class

--- JavaClass2.java
+++ JavaClass2.java
@@ -4,6 +4,7 @@ public class JavaClass2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Second Class");
+		System.out.println("Third Class");
 	}
 
 }
